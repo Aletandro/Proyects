0529d104570466e56187ef7f3feab3dc2bd94b6a
aletandro <1993.lamg@gmail.com>
2021-01-05 20:01:43 -0500

    navbar 2

---
+++ js/main.js
@@ -0,0 +1,4 @@
+let menu= document.querySelector("#menu");
+let toggleicon = document.querySelector("#toggle-icon");
+
+toggleicon.addEventListener("click",() => {menu.classList.toggle("menu-on")});
