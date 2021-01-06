82272a3b28fefa6fa4db9b14aaecdac5c416f97d
aletandro <1993.lamg@gmail.com>
2021-01-05 23:00:30 -0500

    1 video finalizado

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-let menu= document.querySelector("#menu");
+let menu = document.querySelector("#menu");
 let toggleicon = document.querySelector("#toggle-icon");
 
-toggleicon.addEventListener("click",() => {menu.classList.toggle("menu-on")});
+toggleicon.addEventListener("click", ()=>menu.classList.toggle("menu-on"));
